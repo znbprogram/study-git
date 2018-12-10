7af405ac6cbe0262321816bbb59202e04e2c5006
znbprogram <znbtku@gmail.com>
2018-12-11 08:23:02 +0900

    send data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+  return <div>こんにちは{props.name}さん</div>
+}
+
+ReactDOM.render(
+<div>
+<App name = "坂本龍馬" />
+<App name = "西郷どん" />
+</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
